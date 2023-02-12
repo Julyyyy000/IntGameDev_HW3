2dbbb8b07dc87a4ea695ba29b3d5483b8b431c7d
June Shi <jjs833@nyu.edu>
2023-02-12 05:00:23 -0500

    implemented background movement and basic player bouncing

---
+++ Assets/Scripts/BackgroundMovement.cs
@@ -0,0 +1,28 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraPos = camera.GetComponent<Transform>().position;
+        Vector3 newPos = transform.position;
+        newPos.x -= 0.01f;
+        newPos.y = cameraPos.y;
+        if (newPos.x <= -35.40464f)
+        {
+            newPos.x = 35.40464f;
+        }
+
+        transform.position = newPos;
+    }
+}
